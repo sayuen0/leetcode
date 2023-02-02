e1b7333d53e78d28cb13b3db23cf4292880fc736
sayuen0 <sayuen0@gmail.com>
2023-02-02 22:48:00 +0900

    modify script: set file name as problem

--- start.sh
+++ start.sh
@@ -4,9 +4,13 @@ re='^[0-9]+$'
 if ! [[ $1 =~ $re ]] ; then
    echo "error: Not a number" >&2; exit 1
 fi
+if [[ -z $2 ]] ; then
+   echo "error: Empty Problem name" >&2; exit 1
+fi
 
 d=no_$(printf "%02d\n" $1)
+f=$2
 mkdir $d
 
-echo "package main" > $d/main.go
-echo "package main" > $d/main_test.go
+echo "package main" > $d/$f.go
+echo "package main" > $d/"${f}"_test.go
