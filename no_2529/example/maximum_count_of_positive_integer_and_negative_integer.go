347a41a828e22b6a60d51a6f96263632831d0fa9
sayuen0 <sayuen0@gmail.com>
2023-04-02 09:29:31 +0900

    add 2529

--- no_2529/example/maximum_count_of_positive_integer_and_negative_integer.go
+++ no_2529/example/maximum_count_of_positive_integer_and_negative_integer.go
@@ -1,1 +1,40 @@
 package main
+
+func maximumCount(nums []int) int {
+	//find last negative number
+	l, r := 0, len(nums)-1
+	neg := 0
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] < 0 && (mid+1 == len(nums) || nums[mid+1] > -1) {
+			neg = mid + 1 //add 1 because arrays are 0 indexed
+			break
+		} else {
+			if nums[mid] > -1 {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		}
+	}
+	//find first positive number
+	l, r = 0, len(nums)-1
+	pos := 0
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] > 0 && (mid-1 == -1 || nums[mid-1] < 1) {
+			pos = len(nums) - mid
+			break
+		} else {
+			if nums[mid] < 1 {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+	if neg > pos {
+		return neg
+	}
+	return pos
+}
--- no_2529/maximum_count_of_positive_integer_and_negative_integer.go
+++ no_2529/maximum_count_of_positive_integer_and_negative_integer.go
@@ -1,1 +1,17 @@
 package main
+
+func maximumCount(nums []int) int {
+	return count(nums, 0, len(nums)-1)
+}
+
+// 指定された範囲内での
+func count(nums []int, left, right int) int {
+	// 中央のインデックスを決定
+	mid := left + (right - left) - 1
+
+	if nums[mid] == 0 {
+		// 中央値が0
+		// 左右それぞれにどこまで0が続くかで分割する
+	}
+	panic("implement me")
+}
