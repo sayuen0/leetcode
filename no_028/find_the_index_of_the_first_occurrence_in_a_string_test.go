8adbdd74ee31247b91ae3624247c0a8be9dea896
sayuen0 <sayuen0@gmail.com>
2023-04-04 19:02:39 +0900

    add 28

---
+++ no_028/find_the_index_of_the_first_occurrence_in_a_string_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_strStr(t *testing.T) {
+	type args struct {
+		haystack string
+		needle   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{"sadbutsad", "sad"}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := strStr(tt.args.haystack, tt.args.needle); got != tt.want {
+				t.Errorf("strStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
